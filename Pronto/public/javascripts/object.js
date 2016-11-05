6b0c906c829120ac3d0ddbeca4ce34a79cac9b03
alex <HE117908@students.ephec.be>
2016-11-05 21:32:27 +0100

    commit
    objet qui sera reçu de la bdd

--- Pronto/public/javascripts/object.js
+++ Pronto/public/javascripts/object.js
@@ -1,68 +1,83 @@
 var données = {
-    boissons: {
-        elem7: {
-            Categorie: "boisson",
-            Detail: "",
-            Nom: "COFFEE"
-        },
-        elem8: {
-            Categorie: "boisson",
-            Detail: "",
-            Nom: "TEA SERVICE"
-        },
-        elem9: {
-            Categorie: "boisson",
-            Detail: "",
-            Nom: "Vin rouge"
-        },
-        elem10: {
-            Categorie: "boisson",
-            Detail: "",
-            Nom: "Jus de pomme"
-        }
+    tables : {
+        table1 : "table1",
+        table2 : "table2",
+        table3 : "table3",
+        table4 : "bar"
     },
-    plats: {
-        elem1: {
-            Accompagnements: Array[0],
-            Categorie: "entree",
-            Detail: "",
-            Nom: "HEIRLOOM TOMATO & WATERMELON GAZPACHO",
-            Supplements: Array[0]
+    serveurs : {
+        serveur1 : "serveur1",
+        serveur2 : "serveur2",
+        serveur3 : "serveur3",
+        serveur4 : "barman"
+    },
+
+    elements : {
+        boissons: {
+            elem7: {
+                Categorie: "boisson",
+                Detail: "",
+                Nom: "COFFEE"
+            },
+            elem8: {
+                Categorie: "boisson",
+                Detail: "",
+                Nom: "TEA SERVICE"
+            },
+            elem9: {
+                Categorie: "boisson",
+                Detail: "",
+                Nom: "Vin rouge"
+            },
+            elem10: {
+                Categorie: "boisson",
+                Detail: "",
+                Nom: "Jus de pomme"
+            }
         },
-        elem2: {
-            Accompagnements: Array[0],
-            Categorie: "entree",
-            Detail: "",
-            Nom: "PINEAPPLE CARPACCIO, TUNA TARTARE",
-            Supplements: Array[0]
-        },
-        elem3: {
-            Accompagnements: Array[0],
-            Categorie: "plat",
-            Detail: "",
-            Nom: "HOUSE-MADE RICOTTA",
-            Supplements: Array[0]
-        },
-        elem4: {
-            Accompagnements: Array[0],
-            Categorie: "plat",
-            Detail: "",
-            Nom: "CANNED VEGETABLES",
-            Supplements: Array[0]
-        },
-        elem5: {
-            Accompagnements: Array[0],
-            Categorie: "dessert",
-            Detail: "",
-            Nom: "VALRHONA SINGLE ORIGIN TRUFFLE CAKE",
-            Supplements: Array[0]
-        },
-        elem6: {
-            Accompagnements: Array[0],
-            Categorie: "dessert",
-            Detail: "",
-            Nom: "VANILLA BEAN PANNA COTTA",
-            Supplements: Array[0]
+        plats: {
+            elem1: {
+                Accompagnements: [],
+                Categorie: "entree",
+                Detail: "",
+                Nom: "HEIRLOOM TOMATO & WATERMELON GAZPACHO",
+                Supplements: []
+            },
+            elem2: {
+                Accompagnements: [],
+                Categorie: "entree",
+                Detail: "",
+                Nom: "PINEAPPLE CARPACCIO, TUNA TARTARE",
+                Supplements: []
+            },
+            elem3: {
+                Accompagnements: ["sauce tartare","sauce à l'ail","sauce chasseur"],
+                Categorie: "plat",
+                Detail: "",
+                Nom: "HOUSE-MADE RICOTTA",
+                Supplements: ["salade","frites","riz"]
+            },
+            elem4: {
+                Accompagnements: [],
+                Categorie: "plat",
+                Detail: "",
+                Nom: "CANNED VEGETABLES",
+                Supplements: []
+            },
+            elem5: {
+                Accompagnements: [],
+                Categorie: "dessert",
+                Detail: "",
+                Nom: "VALRHONA SINGLE ORIGIN TRUFFLE CAKE",
+                Supplements: []
+            },
+            elem6: {
+                Accompagnements: [],
+                Categorie: "dessert",
+                Detail: "",
+                Nom: "VANILLA BEAN PANNA COTTA",
+                Supplements: []
+            }
         }
     }
 };
