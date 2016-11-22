31709e14fc8a0c0db2cb166015da232bc1d94094
francoismarchal9352 <f.marchal@students.ephec.be>
2016-11-22 10:53:08 +0100

    Ajout queryPlat

--- Pronto/routes/index.js
+++ Pronto/routes/index.js
@@ -15,12 +15,15 @@ var mySqlClient = mysql.createConnection({
 
 var serveurResult = [];
 var tableResult = [];
+var platResult = new Object();
 
 var queryServeur = 'select IdServeur from Serveurs';
 var queryTable = 'select IdTable from Tables';
+var queryPlat = 'SELECT IdPlat, Nom, Prix, NomCatPlats FROM plats ORDER BY NomCatPlats ASC, Nom ASC';
 
 DBQuery(queryServeur,serveurResult,"IdServeur");
 DBQuery(queryTable,tableResult,"IdTable");
+DBQueryPlat(queryPlat,platResult);
 
 
 
@@ -36,11 +39,25 @@ function DBQuery(query,receive,column){
         });
 }
 
+function DBQueryPlat(query,receive){
+    mySqlClient.query(query,
+        function select(error, results, fields) {
+            if ( results.length > 0 )  {
+                for(i in results){
+                    receive[results[ i ]['IdPlat']] = [results[ i ]['Nom'],results[ i ]['Prix'],results[ i ]['NomCatPlats']];
+                }
+
+            } else console.log("Pas de données");
+        });
+}
+
+
+
 /* GET home page. */
 router.get('/', function(req, res,next) {
-    //console.log(serveurResult);
-    //console.log(tableResult);
-    console.log('index');
+    console.log(serveurResult);
+    console.log(tableResult);
+    console.log(platResult);
     var menu = ["Entrées","Plats","Desserts","Boissons"];
     res.render('index',{
         RestaurantName:"Resto",
