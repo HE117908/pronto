6ad5e96da1e6997faf367b69ba567340be8030a8
Dimithree <dimitri.larose@live.be>
2016-12-08 00:59:28 +0100

    Dernière version caisse(pas fini)
    Dernière version caisse(pas fini)

--- Pronto/public/javascripts/CaisseEnregistreuse.js
+++ Pronto/public/javascripts/CaisseEnregistreuse.js
@@ -7,6 +7,7 @@ var idCom = 'commande'+ date; // id de la commande = commande + date en millisec
 var c = {};
 var long = 0;
 var tableTemp;
+var total;
 
 //fonction pour créer la commande avant l'envoye sans id, table, etc (objets boissons + plats)
 function addCom(){
@@ -25,6 +26,7 @@ function passData(data){
     c = data;
     //drawTable();
     ajouteCommande();
+    payerSep();
 }
 
 function payer(x){
@@ -32,12 +34,12 @@ function payer(x){
     var recu = document.getElementById("recu"+x).value;
     var total = document.getElementById("total"+x).innerHTML;
     var res = (recu - total);
-    console.log(total);
     setElem("rendu"+x, "");
     addElem("rendu"+x, res.toFixed(2) + " €");
     cart['idCommande'] = idCom;
     cart['PrixTotal'] = document.getElementById("total"+x).innerHTML;
     cart['typePaiement'] = document.getElementById("typePaiement"+x).value;
+    cart['platsSuspendu'] = document.getElementById("typeSuspendu"+x).value;
     cart['idTable'] = c['idTable'];
     addCart();
     $.ajax({
@@ -52,36 +54,12 @@ function payer(x){
             raz(x);
         }
     });
-    //raz(x);
-}
-
-function suspendu(n) {
-    var type = getElem("typeSuspendu" + n).value;
-    var prix;
-    if(type=="Soupe"){
-        prix = 5;
-    }else{
-        prix = 2;
-    }
-    console.log(type);
-    var ligne = '<tr id="Table ' + n + '">';
-    ligne += '<td>1</td>';
-    ligne += '<td>' + type + '</td>';
-    ligne += '<td class="prix" id="prix'
-    ligne += n
-    ligne += '" value='
-    ligne += prix
-    ligne += '>' + prix + '€ </td>';
-    ligne += '<td>' + '<button class="add" onclick="ajoutePaiement(this.parentNode.parentNode)"> + </button></td>';
-    ligne += '</tr>';
-
-    addElem("Table "+n, ligne);
 }
 
 function raz(x){
-    setElem("totalTVA"+x,"0");
-    setElem("totalHTVA"+x,"0");
-    setElem("total"+x,"0");
+    setElem("totalTVA"+x, 0);
+    setElem("totalHTVA"+x, 0);
+    setElem("total"+x, 0);
     document.getElementById("recu"+x).value = '';
 }
 
@@ -129,7 +107,6 @@ function ajouteCommande(){
         var prixligneB = (qteb*prixb); //prix boissons selon quantité
         totalB= totalB + prixligneB;
     }
-
     //permet de commander boisson sans plats et inversement
     if (isNaN(totalB)){
         totalB = 0;
@@ -137,96 +114,96 @@ function ajouteCommande(){
     if (isNaN(totalP)){
         totalP = 0;
     }
-    var total = (totalB+totalP);
-
-    if(c.idTable == "Table 1") {
-        var oldtt = parseFloat(getElem('total1').textContent);
-        var tt = total+oldtt;
-        var tttva = (tt*6)/100;
-        var tthtva = (tt - tttva);
-        setElem('totalTVA1', tttva);
-        setElem('totalHTVA1', tthtva);
-        setElem('total1', tt);
-    }else if (c.idTable == "Table 2") {
-        var oldtt = parseFloat(getElem('total2').textContent);
-        var tt = total+oldtt;
-        var tttva = (tt*6)/100;
-        var tthtva = (tt - tttva);
-        setElem('totalTVA2', tttva);
-        setElem('totalHTVA2', tthtva);
-        setElem('total2', tt);
-    }else if (c.idTable == "Table 3") {
-        var oldtt = parseFloat(getElem('total3').textContent);
-        var tt = total+oldtt;
-        var tttva = (tt*6)/100;
-        var tthtva = (tt - tttva);
-        setElem('totalTVA3', tttva);
-        setElem('totalHTVA3', tthtva);
-        setElem('total3', tt);
-    }else if (c.idTable == "Table 4") {
-        var oldtt = parseFloat(getElem('total4').textContent);
-        var tt = total+oldtt;
-        var tttva = (tt*6)/100;
-        var tthtva = (tt - tttva);
-        setElem('totalTVA4', tttva);
-        setElem('totalHTVA4', tthtva);
-        setElem('total4', tt);
-    }else if (c.idTable == "Table 5") {
-        var oldtt = parseFloat(getElem('total5').textContent);
-        var tt = total+oldtt;
-        var tttva = (tt*6)/100;
-        var tthtva = (tt - tttva);
-        setElem('totalTVA5', tttva);
-        setElem('totalHTVA5', tthtva);
-        setElem('total5', tt);
-    }else if (c.idTable == "Table 6") {
-        var oldtt = parseFloat(getElem('total6').textContent);
-        var tt = total+oldtt;
-        var tttva = (tt*6)/100;
-        var tthtva = (tt - tttva);
-        setElem('totalTVA6', tttva);
-        setElem('totalHTVA6', tthtva);
-        setElem('total6', tt);
-    }else if (c.idTable == "Table 7") {
-        var oldtt = parseFloat(getElem('total7').textContent);
-        var tt = total+oldtt;
-        var tttva = (tt*6)/100;
-        var tthtva = (tt - tttva);
-        setElem('totalTVA7', tttva);
-        setElem('totalHTVA7', tthtva);
-        setElem('total7', tt);
-    }else if (c.idTable == "Table 8") {
-        var oldtt = parseFloat(getElem('total8').textContent);
-        var tt = total+oldtt;
-        var tttva = (tt*6)/100;
-        var tthtva = (tt - tttva);
-        setElem('totalTVA8', tttva);
-        setElem('totalHTVA8', tthtva);
-        setElem('total8', tt);
-    }else{}
+    total = (totalB+totalP);
+    var oldtt = parseFloat(getElem('total'+c.idTable.substring(6)).textContent);
+    var tt = total+oldtt;
+    var tttva = (tt*6)/100;
+    var tthtva = (tt - tttva);
+    setElem('totalTVA'+c.idTable.substring(6), tttva);
+    setElem('totalHTVA'+c.idTable.substring(6), tthtva);
+    setElem('total'+c.idTable.substring(6), tt);
 }
 
 function ajoutePaiement(tr){
     long++;
     tr.cells[3].innerHTML="&nbsp;"
     tr= tr.parentNode.removeChild(tr);
-    document.getElementById("tabPaiementSepare").appendChild(tr);
+    document.getElementById("tabPaiementSep").appendChild(tr);
     var totPaiementSepare = 0;
     for(var i = 0; i < long; i++){
-        var pri = document.getElementById("tabPaiementSepare").getElementsByTagName("tr")[i].getElementsByTagName("td")[2].innerHTML;
-        totPaiementSepare += parseFloat(pri);
+        var pri = document.getElementById("tabPaiementSep").getElementsByTagName("tr")[i].getElementsByTagName("td")[2].innerHTML;
+        var qte = document.getElementById("tabPaiementSep").getElementsByTagName("tr")[i].getElementsByTagName("td")[0].innerHTML;
+        if(pri.length == 3){
+            pri = pri.substring(0,pri.length-1);
+        }else if(pri.length == 4){
+            pri = pri.substring(0,pri.length-1);
+        }else if(pri.length == 5){
+            pri = pri.substring(0,pri.length-1);
+        }
+        var res = pri*qte;
+        totPaiementSepare += parseFloat(res);
     }
-    setElem("totalPaiementSepare", totPaiementSepare);
+    setElem("totalPaiementSep", totPaiementSepare);
+    setElem("renduPaiementSep", "");
 }
 
 function payerSep() {
-    var idTab = document.getElementById("tabPaiementSepare").getElementsByTagName("tr")[0].id;
-    console.log(idTab);
+    var recuSep = document.getElementById("recuPaiementSep").value;
+    var totalSep = document.getElementById("totalPaiementSep").innerHTML;
+    var resSep = (recuSep - totalSep);
+    addElem("renduPaiementSep", resSep.toFixed(2) + " €");
+    var resteAPayer = total - totalSep;
+    var selection = document.getElementById("tabPaiementSep").getElementsByTagName("tr")[0];
+    //setElem("totalRes1", resteAPayer);
+    /*if(getElem() == "Table 1"){
+        setElem("totalRes1", resteAPayer);
+    }else if(getElem() == "Table 2"){
+        setElem("totalRes2", resteAPayer);
+    }else if(getElem() == "Table 3"){
+        setElem("totalRes3", resteAPayer);
+    }else if(getElem() == "Table 4"){
+        setElem("totalRes4", resteAPayer);
+    }else if(getElem() == "Table 5"){
+        setElem("totalRes5", resteAPayer);
+    }else if(getElem() == "Table 6"){
+        setElem("totalRes6", resteAPayer);
+    }else if(getElem() == "Table 7"){
+        setElem("totalRes7", resteAPayer);
+    }else if(getElem() == "Table 8"){
+        setElem("totalRes8", resteAPayer);
+    }*/
+    setElem("tabPaiementSep", "");
+    setElem("totalPaiementSep", 0);
+    document.getElementById("recuPaiementSep").value = '';
+
 }
 
 function ticket(){
-    var ticket = document.getElementById("tableau1").innerHTML;
+    var ticket = document.getElementById("Table 1").textContent;
     alert(ticket);
+}
+
+function suspendu(n) {
+    var type = getElem("typeSuspendu" + n).value;
+    var prix;
+    if(type=="Soupe"){
+        prix = 5;
+    }else{
+        prix = 2;
+    }
+    console.log(type);
+    var ligne = '<tr id="Table ' + n + '">';
+    ligne += '<td>1</td>';
+    ligne += '<td>' + type + '</td>';
+    ligne += '<td class="prix" id="prix'
+    ligne += n
+    ligne += '" value='
+    ligne += prix
+    ligne += '>' + prix + '€ </td>';
+    ligne += '<td>' + '<button class="add" onclick="ajoutePaiement(this.parentNode.parentNode)"> + </button></td>';
+    ligne += '</tr>';
+
+    addElem("Table "+n, ligne);
 }
 
 function drawTable(){
